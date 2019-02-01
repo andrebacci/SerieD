5d772d7ccc2b9eb8056fc5f28b64777e8c2d25b8
andrebacci <bacigalupo.andrea88@gmail.com>
2019-02-01 20:20:30 +0100

    Aggiunti tutti i frame di risultati e classifiche

--- SerieD/ViewController/LeagueDetailViewController.swift
+++ SerieD/ViewController/LeagueDetailViewController.swift
@@ -34,29 +34,39 @@ class LeagueDetailViewController: UIViewController {
             break;
         case 1:
             frameScore = "<iframe src='https://www.tuttocampo.it/Widget/Risultati/47e84921-2cf6-4738-95fc-989ae78a662a' width='100%' height='100%' scrolling='no' frameborder='0'></iframe>"
+            frameLeague = "<iframe src='https://www.tuttocampo.it/Widget/Classifica/47e84921-2cf6-4738-95fc-989ae78a662a' width='100%' height='100%' scrolling='no' frameborder='0'></iframe>"
             break;
         case 2:
             frameScore = "<iframe src='https://www.tuttocampo.it/Widget/Risultati/06be8744-4806-4baf-89d9-cc47a6ea5f30' width='100%' height='100%' scrolling='no' frameborder='0'></iframe>"
+            frameLeague = "<iframe src='https://www.tuttocampo.it/Widget/Classifica/06be8744-4806-4baf-89d9-cc47a6ea5f30' width='100%' height='100%' scrolling='no' frameborder='0'></iframe>"
             break;
         case 3:
             frameScore = "<iframe src='https://www.tuttocampo.it/Widget/Risultati/ef7eafae-a9e4-47fa-a3f7-da23e25627e1' width='100%' height='100%' scrolling='no' frameborder='0'></iframe>"
+            frameLeague = "<iframe src='https://www.tuttocampo.it/Widget/Classifica/ef7eafae-a9e4-47fa-a3f7-da23e25627e1' width='100%' height='100%' scrolling='no' frameborder='0'></iframe>"
             break;
         case 4:
             frameScore = "<iframe src='https://www.tuttocampo.it/Widget/Risultati/a9c7b6a0-c655-4231-b75e-90dacd12b8ad' width='100%' height='100%' scrolling='no' frameborder='0'></iframe>"
+            frameLeague = "<iframe src='https://www.tuttocampo.it/Widget/Classifica/a9c7b6a0-c655-4231-b75e-90dacd12b8ad' width='100%' height='100%' scrolling='no' frameborder='0'></iframe>"
             break;
         case 5:
             frameScore = "<iframe src='https://www.tuttocampo.it/Widget/Risultati/17688def-3887-4ec6-ac84-b243269d4f98' width='100%' height='100%' scrolling='no' frameborder='0'></iframe>"
+            frameLeague = "<iframe src='https://www.tuttocampo.it/Widget/Classifica/17688def-3887-4ec6-ac84-b243269d4f98' width='100%' height='100%' scrolling='no' frameborder='0'></iframe>"
             break;
         case 6:
             frameScore = "<iframe src='https://www.tuttocampo.it/Widget/Risultati/ae73913f-9f25-4d55-b1eb-c08f6ad50747' width='100%' height='100%' scrolling='no' frameborder='0'></iframe>"
+            frameLeague = "<iframe src='https://www.tuttocampo.it/Widget/Classifica/ae73913f-9f25-4d55-b1eb-c08f6ad50747' width='100%' height='100%' scrolling='no' frameborder='0'></iframe>"
             break;
         case 7:
             frameScore = "<iframe src='https://www.tuttocampo.it/Widget/Risultati/7e80452d-38fb-49e7-aca9-a8cbcb0d8cbf' width='100%' height='100%' scrolling='no' frameborder='0'></iframe>"
+            frameLeague = "<iframe src='https://www.tuttocampo.it/Widget/Classifica/7e80452d-38fb-49e7-aca9-a8cbcb0d8cbf' width='100%' height='100%' scrolling='no' frameborder='0'></iframe>"
             break;
         case 8:
             frameScore = "<iframe src='https://www.tuttocampo.it/Widget/Risultati/a7cb4aa7-be01-423f-8be6-0fdc1290a966' width='100%' height='100%' scrolling='no' frameborder='0'></iframe>"
+            frameLeague = "<iframe src='https://www.tuttocampo.it/Widget/Classifica/a7cb4aa7-be01-423f-8be6-0fdc1290a966' width='100%' height='100%' scrolling='no' frameborder='0'></iframe>"
             break;
         default:
+            frameScore = "<p>ERRORE DURANTE IL CARICAMENTO DEI RISULTATI</p>"
+            frameLeague = "<p>ERRORE DURANTE IL CARICAMENTO DELLA CLASSIFICA</p>"
             break;
         }
         
@@ -65,6 +75,5 @@ class LeagueDetailViewController: UIViewController {
         
         scoreWebView.loadHTMLString(urlScore, baseURL: nil)
         leagueWebView.loadHTMLString(urlLeague, baseURL: nil)
-        //webView.load(request)
     }
 }
