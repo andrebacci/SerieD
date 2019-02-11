0b70f7ce158ec5e181c9592fd4ea642935e03a3d
Andrea Bacigalupo <bacigalupo.andrea88@gmail.com>
2019-02-11 22:59:44 +0100

    Aggiunto ComunicatiViewController

---
+++ SerieD/ViewController/ComunicatiViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ComunicatiViewController.swift
+//  SerieD
+//
+//  Created by Andrea Bacigalupo on 11/02/19.
+//  Copyright © 2019 Andrea Bacigalupo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ComunicatViewController {
+    
+    let urlComunicati = ""
+}
